f9dc6406ea83e926c922ed3ade30e473a4ef0cf5
seansakumoto <106946168+seansakumoto@users.noreply.github.com>
2022-10-17 09:26:42 +0900

    シーンマネージャー追加

---
+++ InvaderGame/InvaderGame/SceneManager.cpp
@@ -0,0 +1,18 @@
+#include"DxLib.h"
+#include"SceneManager.h"
+
+AdstractScene* SceneManager::Update() {
+
+	AdstractScene* p = mScene->Update();
+	if (p != mScene) {
+		delete mScene;
+		mScene = p;
+	}
+
+	return p;
+}
+
+void SceneManager::Draw() const {
+	mScene->Draw();
+
+}
